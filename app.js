28bdcbadfc8ec651a62bab4e409cc86525fc73a4
Pedro Bernardoni <pedrobernardoni56@gmail.com>
2024-09-24 21:25:29 -0300

    :tada initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // vamos agora pegar e colocar uma imagem para poder servir de bonus para animacao do site
+  // pegar a tag image e substituir a imagem
+
+  // vamos primeiro pegar a imagem
+  /*const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-001.png")
+  } else {
+    img.setAttribute("scr", "./assets/avatar.png")
+  }*/
+}
